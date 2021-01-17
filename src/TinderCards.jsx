f8be0a52fd2289921b0f2842799cd5df60d0826b
sammed-sankonatti <myofficialtrack@gmail.com>
2021-01-18 01:20:47 +0530

    bottom part of the tinder page is added

---
+++ src/TinderCards.jsx
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react';
+import './TinderCards.css';
+
+import TinderCard from 'react-tinder-card';
+
+function TinderCards() {
+    return (
+        <div className="tinderCards">
+                
+        </div>
+    )
+}
+
+export default TinderCards;
