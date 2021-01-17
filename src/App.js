6b5757ca21a3407e463ebf155dce7ec882a4e6b4
sammed-sankonatti <myofficialtrack@gmail.com>
2021-01-18 00:58:57 +0530

    header part completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-        <h2> Simple Tinder</h2>
+        <Header />
     </div>
   );
 }
